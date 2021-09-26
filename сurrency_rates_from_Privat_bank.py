fdd75419c348156453df40c6ad148b7f142774a7
Mikhail Yemelyanov <35406483+Mishagit2307@users.noreply.github.com>
2021-09-27 00:08:33 +0300

    Add files via upload

---
+++ сurrency_rates_from_Privat_bank.py
@@ -0,0 +1,15 @@
+import requests
+
+res = requests.get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+json = res.json()
+usd = json[0]
+eur = json[1]
+rur = json[2]
+print("Курсы валют Приват Банка")
+print("===================")
+print(f"Доллар США.       Курс покупки {usd['buy']}. Курс продажи {usd['sale']}.")
+print("===================")
+print(f"Евро.             Курс покупки {eur['buy']}. Курс продажи {eur['sale']}.")
+print("===================")
+print(f"Российский рубль. Курс покупки {rur['buy']}. Курс продажи {rur['sale']}.")
+
